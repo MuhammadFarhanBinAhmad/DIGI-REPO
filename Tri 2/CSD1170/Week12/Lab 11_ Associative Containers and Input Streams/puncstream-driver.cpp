e84890798dd0781cef60f87d554c9c2c5e4b66c6
CheekyKnob <44768125+MuhammadFarhanBinAhmad@users.noreply.github.com>
2023-03-30 21:36:48 +0800

    week 13

--- Tri 2/CSD1170/Week12/Lab 11_ Associative Containers and Input Streams/puncstream-driver.cpp
+++ Tri 2/CSD1170/Week12/Lab 11_ Associative Containers and Input Streams/puncstream-driver.cpp
@@ -1,19 +1,21 @@
-#include <iostream>
-#include <fstream>
-#include <string>
-#include <map>
-#include <vector>
+
 #include "puncstream.hpp"
 
-int main(int argc, char *argv[]) {
-std::ifstream ifs{argv[1]};
-hlp2::punc_stream ps{ifs};
-ps.whitespace(";:,.?!()\"{}<>/&$@#%^*|~"); // note \“ means ”
-ps.case_sensitive(false); // case-insensitive
-std::map<std::string,size_t> histogram;
-// insert words in file into histogram ...
-for (std::vector<std::string> words; ps>>words; ) {
-for (std::string const& word : words) { ++histogram[word]; }
-}
-// print frequency counter as shown in previous examples
+int main(int argc, char *argv[])
+{
+    std::ifstream ifs{argv[1]};
+    hlp2::punc_stream ps{ifs};
+    ps.whitespace(";:,.?!()\"{}<>/&$@#%^*|~"); // note \“ means ”
+    ps.case_sensitive(false);                  // case-insensitive
+    std::map<std::string, size_t> histogram;
+    // insert words in file into histogram ...
+    for (std::vector<std::string> words; ps >> words;)
+    {
+        for (std::string const &word : words)
+        {
+            ++histogram[word];
+        }
+    }
+    hlp2::print(histogram);
+    // print frequency counter as shown in previous examples
 }
