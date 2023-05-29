4331fd435cc2bbe5eec803251e322da00af9d410
CheekyKnob <44768125+MuhammadFarhanBinAhmad@users.noreply.github.com>
2023-05-30 00:24:02 +0800

    week 5

--- Tri 3/Practice/OOP/LibraryManagementSystem/LibraryManagementSystem/Library.h
+++ Tri 3/Practice/OOP/LibraryManagementSystem/LibraryManagementSystem/Library.h
@@ -1,27 +1,17 @@
 #pragma once
 #include "Book.h"
-#include "Library.h"
 
 #include <vector>
 
 class Library
 {
 public:
-	Library();
-	~Library();
 
-	void PrintAllBook();
-	void GetBook();
-	void SetBook();
+	void DisplayAllBook();
+	void DisplayBooks(int64_t ISBN);
+	void SetBookLender(int64_t ISBN,std::string Lender);
+
 
 private:
 	std::vector<Book> list_Book;
 };
-
-Library::Library()
-{
-}
-
-Library::~Library()
-{
-}
--- Tri 3/Practice/OOP/LibraryManagementSystem/LibraryManagementSystem/User.h
+++ Tri 3/Practice/OOP/LibraryManagementSystem/LibraryManagementSystem/User.h
@@ -1,1 +1,15 @@
 #pragma once
+
+#include "Book.h"
+
+#include <vector>
+
+class User
+{
+public:
+	void Display_BooksBorrowed();
+private:
+	std::vector<Book> Book_Borrowed;
+	std::string Name;
+
+};
